da33559085d3c197a299db067aeeab923efeb002
Jony <johongirxolikov@gmail.com>
2022-11-17 13:29:40 +0500

    jss

--- js/main.js
+++ js/main.js
@@ -4,15 +4,15 @@
 // let minut = date.getMinutes();
 // let isOfficeOpen = (hour >= 9 && minut >=0)
 // console.log('Hour:',hour,  'minut',minut);
-let a = 256;
-let b = '256';
+let a = 1;
+let b = null;
 let x = 0 ;
 
-if (b == 256 ||  a > 256) {
+if (b == a ||  a > x) {
 	alert('Правилной значение');
 }
 
-if (x || b == 256) {
+if (x || b == b) {
 	alert('Верной улсловие');
 }
 if (b == x && a > 0 || x == null) {
