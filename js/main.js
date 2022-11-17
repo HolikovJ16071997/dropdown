618527b4188e08fb5aa63c734fab0efac99639a1
Jony <johongirxolikov@gmail.com>
2022-11-17 12:19:17 +0500

    git js

---
+++ js/main.js
@@ -0,0 +1,27 @@
+//&&
+// let date = new Date();
+// let hour = date.getHours();
+// let minut = date.getMinutes();
+// let isOfficeOpen = (hour >= 9 && minut >=0)
+// console.log('Hour:',hour,  'minut',minut);
+let a = 256;
+let b = '256';
+let x = 0 ;
+
+if (b == 256 || b === 256 || a > 256) {
+	alert('Правилной значение');
+}
+
+if ((x || b == 256) && a > 227) {
+	alert('Верной улсловие');
+}
+
+if (b == 256 && a > 0 && x == null) {
+	alert('Null');
+}else{
+	alert('Значение не Null')
+}
+
+if (b === 256 || (b !== 1 && a)) {
+	alert('False');
+}
