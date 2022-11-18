a7b3b7d6918dac7db8a58451b204e9be6f0ab734
Jony <johongirxolikov@gmail.com>
2022-11-18 15:51:17 +0500

    jssыы

--- js/main.js
+++ js/main.js
@@ -4,23 +4,33 @@
 // let minut = date.getMinutes();
 // let isOfficeOpen = (hour >= 9 && minut >=0)
 // console.log('Hour:',hour,  'minut',minut);
-let a = 1;
-let b = null;
-let x = 0 ;
+// let a = 1;
+// let b = null;
+// let x = 0 ;
 
-if (b == a ||  a > x) {
-	alert('не правильной значение');
-}
+// if (b == a ||  a > x) {
+// 	alert('не правильной значение');
+// }
 
-if (x || b == b) {
-	alert('Верной улсловие');
-}
-if (b == x && a > 0 || x == null) {
-	alert('не Null');
+// if (x || b == b) {
+// 	alert('Верной улсловие');
+// }
+// if (b == x && a > 0 || x == null) {
+// 	alert('не Null');
+// }else{
+// 	alert('Null')
+// }
+
+// if (b === 256 || (b !== 1 && a)) {
+// 	alert('False');
+// }
+let alR = prompt('Вам есть 18')
+let age =18;
+if ( alR >= age) {
+	alert('разрешено')
 }else{
-	alert('Null')
-}
+   alert('не рарешено')
+};
 
-if (b === 256 || (b !== 1 && a)) {
-	alert('False');
-}
+2
+
